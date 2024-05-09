9406e11cd0e9d4e5da6d52ffc053ff676bf07b01
raratul <rkhairulislam@gmail.com>
2024-05-09 13:50:37 +0600

    updated models/collection.rb

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,9 @@
 class Collection < ApplicationRecord
   belongs_to :user
+  has_many :items, dependent: :destroy
+  has_many :user_collections, dependent: :destroy
+  has_many :users, through: :user_collections
+
+  def custom_fields
+  end
 end
