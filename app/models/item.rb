8715aed1ed8c293eea3ba7b441cba4a9de4caba1
raratul <rkhairulislam@gmail.com>
2024-05-09 13:52:42 +0600

    updated models/item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :collection
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :users_who_liked, through: :likes, source: :user
 end
