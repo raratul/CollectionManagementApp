232d882dc308f7ece3dd3769bc839d1363f718d7
raratul <rkhairulislam@gmail.com>
2024-05-09 13:58:46 +0600

    updated models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :collections
+         has_many :items, through: :collections
+         has_many :comments
+         has_many :likes
+         has_many :liked_items, through: :likes, source: :item
+         has_many :user_collections
+         has_many :collections_managed, through: :user_collections, source: :collection
 end
