919bf23960fc112f5d96dd8a8bc4ad8f400d8ea2
raratul <rkhairulislam@gmail.com>
2024-05-10 09:14:28 +0600

    fixed authentication

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,19 @@ Rails.application.routes.draw do
     end
   
     resources :comments
+
+    devise_scope :user do
+      get 'users/sign_out' => 'devise/sessions#destroy'
+    end
   
     get 'search', to: 'search#index'
     get 'tag/:tag', to: 'search#tagged', as: :tag
     get 'latest_items', to: 'items#latest_items', as: :latest_items
   
     get 'admin', to: 'admin#index', as: :admin
-    # Define other admin routes
+
+    resources :users, only: [] do
+      resources :collections, controller: 'user_collections', only: [:index]
+    end
   end
   
