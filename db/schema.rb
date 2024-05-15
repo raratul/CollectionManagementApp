ae7c5f995c0ae27e402cb02516f2081945b758b4
raratul <rkhairulislam@gmail.com>
2024-05-15 10:16:32 +0600

    migrated database for language

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_034649) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_041545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_15_034649) do
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.string "theme"
+    t.string "language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
