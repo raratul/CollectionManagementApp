f87c5147c7ab6e05e1370d6156c9986e277f6826
raratul <rkhairulislam@gmail.com>
2024-05-18 10:20:14 +0600

    dropped tag table

--- app/models/tag.rb
+++
@@ -1,4 +0,0 @@
-class Tag < ApplicationRecord
-    has_many :taggings
-    has_many :items, through: :taggings
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_17_142031) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_18_034925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
