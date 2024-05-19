1c4a0c32f7d6ce6406bd63ccc32432a06498d5bb
raratul <rkhairulislam@gmail.com>
2024-05-19 08:55:02 +0600

    updated user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,26 @@
 class User < ApplicationRecord
+  has_many :collections, dependent: :destroy
+  has_many :items, through: :collections
+  has_many :comments, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :theme, inclusion: { in: %w[light dark], message: "%{value} is not a valid theme" }, allow_nil: true
+  validates :language, inclusion: { in: %w[en pl], message: "%{value} is not a valid language" }, allow_nil: true
+
+  def self.first_admin?
+    where(admin: true).count.zero?
+  end
+
+  def active_for_authentication?
+    super && !blocked?
+  end
+
+  def inactive_message
+    !blocked? ? super : :blocked
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
-
-         has_many :collections
-         has_many :items, through: :collections
-         has_many :comments
-         has_many :likes
-         has_many :liked_items, through: :likes, source: :item
-         has_many :user_collections
-         has_many :collections_managed, through: :user_collections, source: :collection
-
-         validates :name, presence: true
-         validates :theme, inclusion: { in: %w[light dark], message: "%{value} is not a valid theme" }, allow_nil: true
-         validates :language, inclusion: { in: %w[en pl], message: "%{value} is not a valid language" }, allow_nil: true
-
-         def self.first_admin?
-           where(admin: true).count.zero?
-         end
-
-         def active_for_authentication?
-          super && !blocked?
-        end
-      
-        def inactive_message
-          !blocked? ? super : :blocked
-        end
 end
