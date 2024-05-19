ed71f43c48901f3f0bba1b53221f97f455f66221
raratul <rkhairulislam@gmail.com>
2024-05-19 17:41:35 +0600

    updated application.js

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ end
 gem "pg_search", "~> 2.3"
 gem 'bootstrap', '~> 5.0.0'
 gem 'acts-as-taggable-on', '~> 5.0'
+gem 'jquery-ui-rails'
+
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
 // Entry point for the build script in your package.json
 //= require jquery
-//= require jquery_ujs
 //= require jquery-ui
-//= require_tree .
 //= require bootstrap
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
