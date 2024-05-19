18a31730cb1d9adc1a35b7a7d8e4e765fbc50596
raratul <rkhairulislam@gmail.com>
2024-05-19 18:26:26 +0600

    updated tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,9 @@
 class TagsController < ApplicationController
+    def index
+      @tags = Tag.pluck(:name)
+      respond_to do |format|
+        format.json { render json: @tags }
+      end
+    end
 end
+  
