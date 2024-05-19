0c79069f36ae79c2fa602bb701aa94c6ef8ab2d3
raratul <rkhairulislam@gmail.com>
2024-05-19 07:57:32 +0600

    generated Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_19_014610) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_19_015641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_19_014610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["collection_id"], name: "index_items_on_collection_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
