c02a88c13f75c3beec5dcf7b0d047291601c3317
raratul <rkhairulislam@gmail.com>
2024-05-19 17:24:06 +0600

    updated application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Entry point for the build script in your package.json
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require_tree .
 //= require bootstrap
 import "@hotwired/turbo-rails"
 import "./controllers"
