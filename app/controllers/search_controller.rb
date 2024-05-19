6aa92d763faa6c623622f69041d7fe458bbcbb39
raratul <rkhairulislam@gmail.com>
2024-05-19 15:06:10 +0600

    updated search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,10 @@
 class SearchController < ApplicationController
+    def index
+      if params[:query].present?
+        @results = Item.search(params[:query])
+      else
+        @results = Item.none
+      end
+    end
 end
+  
