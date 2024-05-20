2a07004ba6d9af5be10597dc1cec93ddd478a7b1
raratul <rkhairulislam@gmail.com>
2024-05-20 07:58:51 +0600

    updated like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,35 @@
 class LikesController < ApplicationController
-
+    before_action :authenticate_user!
+    before_action :set_item
+  
+    def create
+      @like = @item.likes.build(user: current_user)
+      if @like.save
+        respond_to do |format|
+          format.html { redirect_to [@item.collection, @item], notice: 'Item liked.' }
+          format.js
+        end
+      else
+        redirect_to [@item.collection, @item], alert: 'Unable to like item.'
+      end
+    end
+  
+    def destroy
+      @like = @item.likes.find_by(user: current_user)
+      if @like.destroy
+        respond_to do |format|
+          format.html { redirect_to [@item.collection, @item], notice: 'Like removed.' }
+          format.js
+        end
+      else
+        redirect_to [@item.collection, @item], alert: 'Unable to remove like.'
+      end
+    end
+  
+    private
+  
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
 end
+  
