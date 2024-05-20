ebe85d4422b4a4f48903e6bbe19724f7bc726518
raratul <rkhairulislam@gmail.com>
2024-05-20 06:53:19 +0600

    updated comment channel.js

--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -1,15 +1,14 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("CommentsChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load', () => {
+  const commentsElement = document.getElementById('comments')
+  if (commentsElement) {
+    const itemId = commentsElement.dataset.itemId
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    consumer.subscriptions.create({ channel: "CommentsChannel", item_id: itemId }, {
+      received(data) {
+        commentsElement.innerHTML += data.comment_partial
+      }
+    })
   }
-});
+})
