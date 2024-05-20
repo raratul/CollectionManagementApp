feeb3ba42d5b4f1d3592d2781f56ebe41865de01
raratul <rkhairulislam@gmail.com>
2024-05-20 06:47:09 +0600

    updated home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
-    def index
-      @latest_items = Item.order(created_at: :desc).limit(10)
-      @largest_collections = Collection.left_joins(:items).group(:id).order('COUNT(items.id) DESC').limit(5)
-    #   @tag_cloud = Tag.joins(:taggings).group(:name).order('COUNT(taggings.tag_id) DESC').limit(30)
-    end
+  def index
+    @latest_items = Item.order(created_at: :desc).limit(10)
+
+    @top_collections = Collection.left_joins(:items).group(:id).order('COUNT(items.id) DESC').limit(5)
+
+    @tag_cloud = ActsAsTaggableOn::Tag.most_used(20)
+
+    @tag_cloud ||= []
+  end
 end
-  
