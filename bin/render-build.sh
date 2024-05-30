f63785630e5354a068cb04c27759ee9d682d201d
raratul <rkhairulislam@gmail.com>
2024-05-30 15:02:48 +0600

    new update for deploying on render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
-# exit on error 
+# !/usr/bin/env bash
+# exit on error
 set -o errexit
 
-chmod +x bin/rails
-
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
