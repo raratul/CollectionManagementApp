ca26cee22c36334fd2614b93fac96ef507918391
raratul <rkhairulislam@gmail.com>
2024-05-30 11:59:37 +0600

    updated render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
-# exit on error
+# exit on error 
 set -o errexit
 
 chmod +x bin/rails
 
 bundle install
-
 bundle exec rails assets:precompile
-
 bundle exec rails assets:clean
+bundle exec rails
 
-bin/rails db:environment:set RAILS_ENV=production
-
-rake db:migrate
-
+bin/rails db:environment:set RAILS_ENV=production 
+rake DISABLE_DATABASE_ENVIRONMENT_CHECK=1 db:migrate:reset
 RAILS_ENV=test rake db:migrate
