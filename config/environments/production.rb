815d20374716cbdf14115c41a5ae2d9aac1fa6ea
raratul <rkhairulislam@gmail.com>
2024-05-31 16:04:02 +0600

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.cache_classes = true
-  config.cache_store = :mem_cache_store
 end
