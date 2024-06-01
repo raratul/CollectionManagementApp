a9292023ede57ae292a8e69b41e24e7220990801
raratul <rkhairulislam@gmail.com>
2024-06-01 12:16:19 +0600

    updated application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,8 @@
 // Entry point for the build script in your package.json
 
+import "./controllers"
+import * as bootstrap from "bootstrap"
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
-
-
-import "./controllers"
-import * as bootstrap from "bootstrap"
-
