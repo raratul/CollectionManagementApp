a8af4c321e4a7bbf99ee747d191a285a1b02fb9a
raratul <rkhairulislam@gmail.com>
2024-06-10 21:10:57 +0600

    implemented jira integration

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -38,7 +38,7 @@ class TicketsController < ApplicationController
 
   def create_issue_via_bash(summary, description)
     command = <<-BASH
-      curl -u #{ENV['ATLASSIAN_USERNAME']}:#{Rails.application.secrets.atlassian_api_token} \
+      curl -u #{ENV['ATLASSIAN_USERNAME']}:#{Rails.application.secrets.jira_api_token} \
            -X POST \
            --data '{
              "fields": {
