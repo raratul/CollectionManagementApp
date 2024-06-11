4c6946e9a640db3b8846b99427a1d782a19f9003
raratul <rkhairulislam@gmail.com>
2024-06-11 12:53:42 +0600

    updated jira integration

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-
+require 'dotenv/load' if Rails.env.development? || Rails.env.test? || Rails.env.production?
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
